b8d838bf4d9e39a5f4adc994a138ad4d47489238
kawa11 <cmcc@live.hk>
2020-05-26 17:34:23 +0800

    Update crawler.py

--- service/crawler.py
+++ service/crawler.py
@@ -80,15 +80,11 @@ class Crawler:
         for area in area_information:
             area['comment'] = area['comment'].replace(' ', '')
 
-            # 由于城市具有其他属性，
-            # 检查相同文档时，不应使用此部分。
-
             cities_backup = area.pop('cities')
 
             if self.db.find_one(collection='DXYArea', data=area):
                 continue
 
-            #如果此文档不在当前数据库中，则将此属性重新插入到文档中。
             area['cities'] = cities_backup
 
             area['countryName'] = '中国'
@@ -118,13 +114,9 @@ class Crawler:
                 country.pop('id')
                 country.pop('tags')
                 country.pop('sort')
-                # 丁香园有大量重复的数据,
-                # 值虽然相同，但是修改时间不同.
-                # 时间不同，弹出重复的数据.
+            
                 country.pop('modifyTime')
-                # 即使值相同，createTime也不同。
-                # 最初，createTime代表该区域对该病毒的首次诊断，
-                # 但国外信息似乎有所不同。
+
                 country.pop('createTime')
                 country['comment'] = country['comment'].replace(' ', '')
             except KeyError:
@@ -132,9 +124,7 @@ class Crawler:
             country.pop('countryType')
             country.pop('provinceId')
             country.pop('cityName')
-            # 原来的provinceShortName是空白字符串
             country.pop('provinceShortName')
-            # 将continents重命名为continentName
             country['continentName'] = country.pop('continents')
 
             if self.db.find_one(collection='DXYArea', data=country):
